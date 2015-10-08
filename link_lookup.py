686c0d275a661e6023a662f140a34eb8702b4a29
Mark Chamberlain <dcbdmb@gmail.com>
2015-10-08 19:52:07 -0400

    initial add of code to take long url and check if there is a bitly

---
+++ link_lookup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import requests
+import json
+import getopt
+import sys
+
+def check_input():
+    #print "ARGV:",sys.argv[0:]
+
+    options, remainder = getopt.gnu_getopt(sys.argv[1:], 'u:', ['url='])
+
+    #print 'Options:',options
+
+    opt_count = 0
+
+    for opt,arg in options:
+        opt_count = opt_count + 1
+        if opt in ('-u','--url'):
+            long_url = arg
+        else:
+            print 'bad option given'
+            opt_count = opt_count - 1
+
+    if (opt_count == 0):
+        print "usage is:",sys.argv[0]
+        print '\t-u, --url'
+        exit()
+
+    return (long_url)
+
+long_url = check_input()
+
+config = {}
+execfile("bitly_config.py",config)
+
+link_params = {'access_token':config['token'], 'url':long_url} 
+
+link_lookup = 'https://api-ssl.bitly.com/v3/user/link_lookup'
+response = requests.get(link_lookup, params=link_params, verify=True)
+
+#output = response.json()
+
+output = response.json()['data']['link_lookup'][0]
+if 'error' in output.keys():
+    print "The URL:\n",output['url'],"\n WAS NOT FOUND!!!"
+else:
+    print "The URL:\n",output['url'],"\n WAS already saved as:",output['link']
